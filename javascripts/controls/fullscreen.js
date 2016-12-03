0309c2f00e88382e5321a40533e4e200340984bb
tsekitsi <tsekitsi@grinnell.edu>
2016-12-02 22:41:16 -0600

    All maps

--- javascripts/controls/fullscreen.js
+++
@@ -1,5 +0,0 @@
-var fullScreenChar = document.createElement('span');
-fullScreenChar.className = 'ui-icon-arrowthick-2-ne-sw';
-var myFullScreen = new ol.control.FullScreen({
-    label: fullScreenChar
-});
