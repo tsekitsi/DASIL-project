17443fb5bd3f62cdaaedd73af7f4684543e3d86e
tsekitsi <tsekitsi@grinnell.edu>
2016-12-02 22:42:50 -0600

    All maps

---
+++ javascripts/maps/map54.js
@@ -0,0 +1,13 @@
+var base1954 = new ol.layer.Image({
+  opacity: 0.875,
+  source: new ol.source.ImageStatic({
+    url: 'images/base_layers/1954.jpg',
+    projection: map.getView().getProjection(),
+    imageExtent: ol.extent.applyTransform([-92.71768, 41.74621, -92.72255, 41.756],
+      ol.proj.getTransform("EPSG:4326", "EPSG:3857"))
+  })
+});
+
+function fill_markers_of_1954() {
+
+}
---
+++ javascripts/maps/map59.js
@@ -0,0 +1,13 @@
+var base1959 = new ol.layer.Image({
+  opacity: 0.875,
+  source: new ol.source.ImageStatic({
+    url: 'images/base_layers/1959.jpg',
+    projection: map.getView().getProjection(),
+    imageExtent: ol.extent.applyTransform([-92.71768, 41.74621, -92.72255, 41.75196],
+      ol.proj.getTransform("EPSG:4326", "EPSG:3857"))
+  })
+});
+
+function fill_markers_of_1959() {
+
+}
