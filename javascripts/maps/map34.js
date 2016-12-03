876e99955c2a95b37ebb98e52cdf5ee05afd9208
tsekitsi <tsekitsi@grinnell.edu>
2016-12-02 22:36:34 -0600

    All maps

--- javascripts/maps/map34.js
+++ javascripts/maps/map34.js
@@ -1,7 +1,7 @@
 var base1934 = new ol.layer.Image({
   opacity: 0.875,
   source: new ol.source.ImageStatic({
-    url: 'images/base_layers/croped_scan34.jpg',
+    url: 'images/base_layers/1934.jpg',
     projection: map.getView().getProjection(),
     imageExtent: ol.extent.applyTransform([-92.71768, 41.74621, -92.72255, 41.75196],
       ol.proj.getTransform("EPSG:4326", "EPSG:3857"))
--- javascripts/maps/map54.js
+++ javascripts/maps/map54.js
@@ -1,7 +1,7 @@
 var base1954 = new ol.layer.Image({
   opacity: 0.875,
   source: new ol.source.ImageStatic({
-    url: 'images/base_layers/cropped_1954.jpg',
+    url: 'images/base_layers/1954.jpg',
     projection: map.getView().getProjection(),
     imageExtent: ol.extent.applyTransform([-92.71768, 41.74621, -92.72255, 41.756],
       ol.proj.getTransform("EPSG:4326", "EPSG:3857"))
